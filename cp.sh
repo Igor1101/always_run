a7317d7546dde52cef600a5ff5e5c26ef1d67dd4
Igor <igor.muravyov.pz.2021@ukr.net>
2022-03-13 08:46:09 +0200

    add readme+ add remoute autorestart"

--- cp.sh
+++ cp.sh
@@ -7,9 +7,16 @@ file_hosts='hosts.txt'
 n=1
 while read line 
 do
-        echo "linenum(host): $n:$line"
-        for filename in $(ls)
-        do 
-            curl --insecure --user "$USERNAME:$PASSWORD" -T $filename  "sftp://$line/home/$USERNAME/always_run/$filename" --ftp-create-dirs
-        done
+    host=$line
+    echo "linenum(host): $n:$host"
+    # copy all the files
+    for filename in $(ls)
+    do 
+        curl --insecure --user "$USERNAME:$PASSWORD" \
+             -T $filename  "sftp://$host/home/$USERNAME/always_run/$filename"\
+             --ftp-create-dirs
+    done
+    # run init
+    sshpass -p $PASSWORD ssh $USERNAME@$host \
+        'cd always_run; ./init.sh'
 done < $file_hosts
--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-sudo chmod u+x ./run.sh
-sudo chmod u+x ./user_code.sh
-tmux new-session -d -s "always_run"  ./run.sh
+tmux kill-session -t always_run
+tmux new-session -d -s "always_run"  bash run.sh

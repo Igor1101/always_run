0f00741050acac6ef981ca994e3e741875c3f882
Igor <igor.muravyov.pz.2021@ukr.net>
2022-03-27 15:10:27 +0300

    add option - key set

--- conf.sh
+++ conf.sh
@@ -3,3 +3,10 @@
 PAUSE_TIME=10
 # session name to use
 TMUX_SESSION=always_run
+# if using key for authentification(cpk.sh, killallk.sh, statusk.sh)
+# you can write path to it here:
+# note if key as parameter used when calling any of such file
+# (cpk.sh, killallk.sh, statusk.sh) key parameter is used instead of 
+# DEFAULT_KEY_PATH. If DEFAULT_KEY_PATH is empty, KEY_PATH(as parameter used).
+# example DEFAULT_KEY_PATH=/aws/key_pair_AWS.pem
+DEFAULT_KEY_PATH=
